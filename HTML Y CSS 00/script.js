247375bae7f0711d39abaed07c088a664ab4b8bf
EliasRC13 <eliasrc134531@gmail.com>
2026-06-08 02:15:08 -0300

    Inclusion de archivo JS en HTML y CSS 00

---
+++ HTML Y CSS 00/script.js
@@ -0,0 +1,6 @@
+const boton = document.querySelector("#boton-importante")
+
+boton.addEventListener('click', function(){
+    boton.textContent = 'Aplicado!';
+    boton.style.backgroundColor = '#22c55e' ;
+})
