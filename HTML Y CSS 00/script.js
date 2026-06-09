7beb28951d5468231b8de838e1a9affe30c7efb6
EliasRC13 <eliasrc134531@gmail.com>
2026-06-09 10:11:17 -0300

    Cambio de forEach a Delegacion de Eventos

--- HTML Y CSS 00/script.js
+++ HTML Y CSS 00/script.js
@@ -1,8 +1,24 @@
-const botones = document.querySelectorAll("#boton-importante")
+//const botones = document.querySelectorAll(".button-apply-job")
 
-botones.forEach( function (boton) {
-boton.addEventListener ('click',  function(){
-    boton.textContent = 'Aplicado!';
-    boton.style.backgroundColor = '#22c55e' ;
-})
-})
+//botones.forEach( function (boton) {
+//boton.addEventListener ('click',  function(){
+  //  boton.textContent = 'Aplicado!';
+  //  boton.style.backgroundColor = '#22c55e' ;
+  //  boton.disabled = true;
+//})
+//})
+
+const container = document.querySelector('.jobs-listings');
+
+container.addEventListener('click', function (event) {
+
+  if (event.target.classList.contains('button-apply-job')) {
+
+    const boton = event.target;
+
+    boton.textContent = '¡Aplicado!';
+    boton.style.backgroundColor = '#22c55e';
+    boton.disabled = true;
+  }
+
+});
